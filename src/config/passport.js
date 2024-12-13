556ba09c8a30c3a0f9917e834d8dca1465a18c19
mmelton12 <mattmelton@gmail.com>
2024-12-13 13:45:01 -0500

    updates

--- src/config/passport.js
+++ src/config/passport.js
@@ -8,7 +8,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: '/api/auth/google/callback',
+            callbackURL: 'https://api.mattymeltz.com/api/auth/google/callback',
             proxy: true
         },
         async (accessToken, refreshToken, profile, done) => {
