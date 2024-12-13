4facd833688ed1cf1397233c324727e66970837c
mmelton12 <mattmelton@gmail.com>
2024-12-13 18:23:46 -0500

    Auth fixes

--- src/config/passport.js
+++ src/config/passport.js
@@ -8,7 +8,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: 'https://api.mattymeltz.com/auth/google/callback/flowName=GeneralOAuthFlow',
+            callbackURL: 'https://api.mattymeltz.com/auth/google/callback',
             proxy: true
         },
         async (accessToken, refreshToken, profile, done) => {
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -12,7 +12,7 @@ router.get('/google',
     passport.authenticate('google', { scope: ['profile', 'email'] })
 );
 
-router.get('/google/callback/flowName=GeneralOAuthFlow',
+router.get('/google/callback',
     passport.authenticate('google', { session: false }),
     async (req, res) => {
         try {
